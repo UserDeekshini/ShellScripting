47537555cae8f4f4aede739219bee7c994f6453e
Kukkunoor Gopal <254207@cognizant.com>
2022-07-12 12:51:22 +0530

    ch

---
+++ 02-print.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#colors  foreground  Background
+#Red        31           41
+#Green      32           42
+#Yellow     33           43
+#Blue       34           44
+#Magenta    35           45
+#cyan       36           46
+
+# echo -e "\e[colorCodem   Message \e[0m"
+echo -e "\e[31m   Message \e[0m"
+
+echo -e "\e[41;36m  Message \e[0m" 
