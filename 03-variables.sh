3e8123717a925167e60ea4413c1caf7b486fc9ec
Kukkunoor Gopal <254207@cognizant.com>
2022-07-12 17:43:47 +0530

    changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,3 +2,14 @@
 DATE_COMMAND=$(date +%F)
 echo "Good Morning Today's date is $DATE_COMMAND"
 echo "total number of user's logged in now is $(who |wc -l)"
+
+echo $0 
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+echo $9
