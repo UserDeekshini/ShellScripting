e10ded2fc2454be222c2d728e08010e02460cde5
Kukkunoor Gopal <254207@cognizant.com>
2022-07-12 12:54:15 +0530

    c

--- 02-print.sh
+++ 02-print.sh
@@ -9,6 +9,12 @@
 #cyan       36           46
 
 # echo -e "\e[colorCodem   Message \e[0m"
-echo -e "\e[31m   Message \e[0m"
+echo -e "\e[31m   I am printing RED color \e[0m"
+echo -e "\e[32m   I am printing GREEN color \e[0m"
+echo -e "\e[33m   I am printing YELLOW color \e[0m"
+echo -e "\e[34m   I am printing BLUE color \e[0m"
+echo -e "\e[35m   I am printing MAGENTA color \e[0m"
+echo -e "\e[36m   I am printing CYAN color \e[0m"
 
-echo -e "\e[41;36m  Message \e[0m" 
+# echo -e "\e[background;colorCodem   Message \e[0m"
+echo -e "\e[41;36m  I am printing RED backgroung color \e[0m" 
